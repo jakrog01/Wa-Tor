cf5cf923564422dd2acc1d4f9233117b1ad0249f
_rogala <jk.rogala@student.uw.edu.pl>
2024-05-23 11:00:12 +0200

    Add RespectToPreyInitStrategy

--- Wa-TorAnalyzer.py
+++ Wa-TorAnalyzer.py
@@ -1,16 +1,17 @@
 from AnalysisScripts.Strategies.PlotOscilationsStrategy import PlotOscilationInTimeStrategy
 from AnalysisScripts.Strategies.RespectToStrategies.RespectToBStrategy import RespectToBStrategy
 from AnalysisScripts.Strategies.RespectToStrategies.RespectToAreaSizeStrategy import RespectToAreaSizeStrategy
+from AnalysisScripts.Strategies.RespectToStrategies.RespectToPreyInitStrategy import RespectToPreyInitStrategy
 
 if __name__ == '__main__':
     threads_count = 5
     count_to_average = 2
     iteration_per_step = 1000
 
-    strategy = RespectToAreaSizeStrategy(False, threads_count, count_to_average, iteration_per_step)
+    strategy = RespectToPreyInitStrategy(False, threads_count, count_to_average, iteration_per_step)
 
-    area_sizes_tuple = set([1,2,3,4,5,6,7,8,9,10])
-    init_prey_populations_tuple = set([60])
+    area_sizes_tuple = set([25])
+    init_prey_populations_tuple = set([60,50,40,30,20,10,1])
     init_predators_populations_tuple = set([4])
     a_params_tuple = set([5])
     b_params_tuple = set([100,50])
